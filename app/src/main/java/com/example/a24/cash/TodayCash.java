a711025e1ee6b9912c359587035dc2ba9bd24e23
izemir <laruscrococat@gmail.com>
2018-07-20 15:25:51 +0300

    adding list

--- app/src/main/java/com/example/a24/cash/TodayCash.java
+++ app/src/main/java/com/example/a24/cash/TodayCash.java
@@ -3,6 +3,7 @@ package com.example.a24.cash;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +15,12 @@ public class TodayCash extends AppCompatActivity {
         setContentView(R.layout.activity_today_cash);
     }
 
-    OneBuy first;
+    OneBuy first = new OneBuy(24, "IceCream"),
+            second = new OneBuy(144,"Chips");
 
-    ArrayList<OneBuy> list = new ArrayList<OneBuy>();
+    public ArrayList<OneBuy> list = new ArrayList<OneBuy>();
+
+
+
+
 }
